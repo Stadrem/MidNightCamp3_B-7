c015c5f5c253e79dda96dd8bfad18f40d07d4ecf
ajune010 <cang6339@naver.com>
2024-08-20 23:40:58 +0900

    Merge branch 'main' of https://github.com/Stadrem/MidNightCamp3_B-7

---
+++ Assets/Script/Jueun/CamFollow.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    
+    // 목표가 될 트랜스폼 컴포넌트
+    public Transform target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 카메라의 위치를 목표 트랜스폼의 위치에 일치시키기
+        transform.position = target.position;
+        
+        
+        
+    }
+    
+    
+}
+
+
+
+
+
+
