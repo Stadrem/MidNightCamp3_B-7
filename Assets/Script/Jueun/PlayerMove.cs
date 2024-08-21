d234389f278b8a05b7f0f308b5fd7b6094555492
ajune010 <cang6339@naver.com>
2024-08-21 10:57:05 +0900

    씬, 사운드 폴더 추가

--- Assets/Script/Jueun/PlayerMove.cs
+++ Assets/Script/Jueun/PlayerMove.cs
@@ -111,6 +111,9 @@ public class Player : MonoBehaviour
         transform.position += dir * moveSpeed * Time.deltaTime;
         cc.Move(dir * moveSpeed * Time.deltaTime);
         
+
+        
+        
         
     }
     
