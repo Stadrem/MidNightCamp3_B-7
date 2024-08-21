ca76544d6c2ed146a36ffe9704f13bbf72f2b114
FortressforTrees <rlarlcks085@gmail.com>
2024-08-21 22:15:35 +0900

    ..l
    lll

--- Assets/Script/KGC/PastResponse.cs
+++ Assets/Script/KGC/PastResponse.cs
@@ -13,8 +13,6 @@ public class PastResponse : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        rList.Add(responses.text);
         
         rList.Add(responses.text);
         responses.text = rList[page];
